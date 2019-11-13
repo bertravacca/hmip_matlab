be2f228d8fb3d9c980f6294fce119ada431659f1
Bertrand Travacca <bertrand.travacca@berkeley.edu>
2019-11-13 15:09:16 -0800

    added function to generate random qp parameters

--- examples/random_QPs.m
+++ examples/random_QPs.m
@@ -6,26 +6,7 @@ addpath(char(join(str(1:length(str)-1),'/')));
 
 % define problem parameters at random
 n=20;
-beta=0.4;
-Z=rand(n,1);
-binary_indicator=ones(n,1).*(Z<=beta); clear Z;
-density=0.4;
-A=rand(n,n).*(rand(n,n)>density);
-V=orth(A);
-D=diag(n*rand(n,1));
-Q=V'*D*V; clear V D A 
-Q=(0.5/n)*(Q'+Q);
-B=rand(n,n).*(rand(n,n)>density);
-sigma=B'*B;
-q=(1/n)*mvnrnd(zeros(n,1),sigma)'; clear B sigma
-gamma=0.8;
-m_eq=floor(gamma*rand()*n);
-m_ineq=floor(gamma*rand()*n);
-Aeq=rand(m_eq,n).*(rand(m_eq,n)>density);
-A=rand(m_ineq,n).*(rand(m_ineq,n)>density);
-z=(1-binary_indicator).*rand(n,1)+binary_indicator.*(rand(n,1)>0.5);
-beq=Aeq*z;
-b=A*z+10^-3*rand(m_ineq,1);
+[binary_indicator,Q,q]=random_qp_parameters('no_constraints',n,0.4);
 lb=0;
 ub=1;
 
