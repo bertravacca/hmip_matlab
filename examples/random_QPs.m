a6b458ff0d9a6c6916aae222245615f5329e7a17
Bertrand Travacca <bertrand.travacca@berkeley.edu>
2019-11-13 15:00:34 -0800

    improved example random QPs

--- examples/random_QPs.m
+++ examples/random_QPs.m
@@ -14,10 +14,10 @@ A=rand(n,n).*(rand(n,n)>density);
 V=orth(A);
 D=diag(n*rand(n,1));
 Q=V'*D*V; clear V D A 
-Q=0.5*(Q'+Q);
+Q=(0.5/n)*(Q'+Q);
 B=rand(n,n).*(rand(n,n)>density);
 sigma=B'*B;
-q=mvnrnd(zeros(n,1),sigma)'; clear B sigma
+q=(1/n)*mvnrnd(zeros(n,1),sigma)'; clear B sigma
 gamma=0.8;
 m_eq=floor(gamma*rand()*n);
 m_ineq=floor(gamma*rand()*n);
@@ -29,7 +29,19 @@ b=A*z+10^-3*rand(m_ineq,1);
 lb=0;
 ub=1;
 
-% create ctype string for cplex
+% hmip solver
+objective=@(x) 0.5*x'*Q*x+q'*x;
+gradient=@(x) Q*x+q;
+problem=problemHMIP('objective',objective,'gradient',gradient,'size',n,'binary_index',binary_indicator,'lb',lb,'ub',ub);
+options=OptionsHMIP('num_iterations_max',10^3,'keep_hopfield_trajectory',1,'activation_type','pwl','direction_method','binary');
+solver=solverHMIP('problem',problem,'options',options);
+solver=solver.main_hopfield;
+% projected gradient descent
+[x_pgd,fval_pgd,step_size_pgd]=solver.projected_gradient_descent;
+% quadprog
+options = optimset('Display', 'off');
+[x_qp,fval_qp]=quadprog(Q,q,[],[],[],[],lb*ones(n,1),ub*ones(n,1),[],options);
+% cplexmiqp
 for k=1:n
     if binary_indicator(k)==1
         ctype(k)='B';
@@ -37,27 +49,19 @@ for k=1:n
         ctype(k)='C';
     end
 end
+[x_cplx,fval_cplx]=cplexmiqp(Q, q, [], [], [], [],[], [], [], lb*ones(n,1), ub*ones(n,1), ctype);
 
-
-% use hmip solver
-objective=@(x) 0.5*x'*Q*x+q'*x;
-gradient=@(x) Q*x+q;
-problem=problemHMIP('objective',objective,'gradient',gradient,'size',n,'binary_index',binary_indicator,'lb',lb,'ub',ub);
-options=OptionsHMIP('num_iterations_max',10^3,'keep_hopfield_trajectory',1,'activation_type','pwl','direction_method','binary');
-solver=solverHMIP('problem',problem,'options',options);
-solver=solver.main_hopfield;
-[x,fval_pgd,step_size]=solver.projected_gradient_descent;
-[x_qp,fval_qp]=quadprog(Q,q,[],[],[],[],lb*ones(n,1),ub*ones(n,1));
-options = cplexoptimset('optimalitytarget', 1);
-[x,fval]=cplexmiqp(Q, q, [], [], [], [],[], [], [], lb*ones(n,1), ub*ones(n,1), ctype, [],options);
-
+% plot fval for the different methods
 figure(1)
 semilogx(solver.fval,'b')
 hold on
 semilogx(fval_pgd,'r')
 semilogx(fval_qp*ones(max(length(solver.fval),length(fval_pgd)),1),'r--')
-semilogx(fval*ones(max(length(solver.fval),length(fval_pgd)),1),'b--')
+semilogx(fval_cplx*ones(max(length(solver.fval),length(fval_pgd)),1),'b--')
 hold off
+title('Objective function value across iterations')
+xlabel('# of iterations')
+ylabel('fval')
 
 %figure(2)
 %semilogx(100*solver.step_size,'b')
